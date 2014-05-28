e9f6510d745262aa0435a04d5051f72cd8d98c8e
Ales Justin <ales.justin@gmail.com>
2014-05-28 15:02:20 +0200

    Add logging.

--- src/main/java/org/openblend/gae123/HelloServlet.java
+++ src/main/java/org/openblend/gae123/HelloServlet.java
@@ -1,6 +1,7 @@
 package org.openblend.gae123;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +12,15 @@ import javax.servlet.http.HttpServletResponse;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class HelloServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(HelloServlet.class.getName());
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
+        final String name = req.getParameter("name");
+
+        if (name != null) {
+            log.info(String.format("[LOG] name = %s", name));
+        }
+
         resp.getWriter().write("Hello " + name + "!");
     }
 }
